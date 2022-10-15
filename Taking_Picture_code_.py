5ec60b5210b0393ec51e9e3c3b66aafbb1c92e30
tanmaydeokar <100110322+tanmaydeokar@users.noreply.github.com>
2022-10-16 01:24:05 +0530

    Create Taking_Picture_code_.py

---
+++ Taking_Picture_code_.py
@@ -0,0 +1,14 @@
+import picamera
+from time import sleep
+
+camera=picamera.PiCamera()
+camera.capture('Img1.jpg')
+
+sleep(1)
+camera.capture('img8.jpg')
+
+camera.start_preview()
+camera.brightness=75
+sleep(3)
+
+camera.stop_preview()
